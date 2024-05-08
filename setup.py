a05da2a7afe955abfd2f550f3c3187d88d126969
YangChen <1140377596@qq.com>
2024-05-08 16:16:16 +0800

    0.4.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vis_analysis_torch',
-    version='0.3.1',
+    version='0.4.2',
     packages=find_packages(),
     install_requires=[
         "numpy",
